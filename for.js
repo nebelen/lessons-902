eb8d9fa63621167d330c1df9e1a9879f63a46db5
nebelen <nebelenchuk@gmail.com>
2019-03-24 13:53:50 +0200

    урок 5

---
+++ for.js
@@ -0,0 +1,16 @@
+function checkAge(age) {
+    if (age > 18) {
+        // длинное тело
+
+
+    }
+}
+
+
+function checkAge1(age) {
+    if (age <= 18) {
+        return;
+    }
+
+    // длинное тело
+}
