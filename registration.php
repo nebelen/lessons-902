4c88b0b6f1af6168c54a3e1d540be6645f2390b6
nebelen <nebelenchuk@gmail.com>
2019-03-25 11:55:26 +0200

    урок 8 формы

---
+++ registration.php
@@ -0,0 +1,17 @@
+<?php
+
+<!DOCTYPE html>
+<html lang="ru">
+<head>
+    <meta charset="utf-8">
+    <title>Form class</title>
+    <link rel="stylesheet" href="main.css">
+</head>
+
+<body>
+<?= $form->render() ?>
+
+<?= $formGet->render() ?>
+
+</body>
+</html>
