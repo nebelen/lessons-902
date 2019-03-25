0c0aa7cc5ca2dc1833b2e4d3f0561f5c38f31dd8
nebelen <nebelenchuk@gmail.com>
2019-03-25 14:51:25 +0200

    домашка 8 урок

--- task1.php
+++ task1.php
@@ -1,24 +1,25 @@
 <?php
-# Путь к файлу
+// структуру кода нашел в инете, подкоректировал под нашу задачу
+// Путь к файлу
 $file_name = "users.txt";
 
-# Считываем информацию по строкам
+// Считываем информацию по строкам
 $data = file( $file_name );
 ?>
 <table border="1">
     <?php
-    # В цикле обходим массив данных
-    foreach( $data as $value ):
+    // В цикле обходим массив данных
+    foreach( $data as $value ) {
 
-        # Разбиваем строку по '\t'
-        $value = explode("\t", $value );
+        // Разбиваем строку по '\t'
+        $value = explode("\t", $value);
 
         ?>
         <tr>
-            <td><?=$value[0]?></td>
-            <td><?=$value[1]?></td>
+            <td><?= $value[0] ?></td>
+            <td><?= $value[1] ?></td>
         </tr>
-    <?php
-    endforeach;
+        <?php
+    }
     ?>
 </table>
